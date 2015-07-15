2e84ddcf2ffe3e451dc68b08f8376bed656de434
wumiliu <wumi_liu@foxitsoftware.com>
2015-07-15 15:33:23 +0800

    编译着色器工具

---
+++ D3D11Demo/Resources/BuildShader.cmd
@@ -0,0 +1,7 @@
+
+Tools\\CompileShader.exe Shaders\\DynamicPrimitiveVS.hlsl   Shaders\\DynamicPrimitiveVS.shader ColorVertexShader vs_4_0
+Tools\\CompileShader.exe Shaders\\DynamicPrimitivePS.hlsl   Shaders\\DynamicPrimitivePS.shader ColorPixelShader ps_4_0
+
+Light.cmd
+
+pause
