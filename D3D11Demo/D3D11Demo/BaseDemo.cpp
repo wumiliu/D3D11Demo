66c76dd179cf7b63856357ebdf6b78f6fc07f668
wumiliu <wumi_liu@foxitsoftware.com>
2015-07-15 18:09:25 +0800

    曲线测试

--- D3D11Demo/D3D11Demo/BaseDemo.cpp
+++ D3D11Demo/D3D11Demo/BaseDemo.cpp
@@ -3,12 +3,18 @@
 #include "Texture/DDSTextureLoader.h"
 #include "VertexTypes.h"
 #include "Shader/ShaderManager.h"
+#include "Cure/Curve.h"
+#include "Cure/spline.h"
 BaseDemo::BaseDemo(HINSTANCE hInstance, int nWidth /*= 1024*/, int nHeight /*= 600*/)
 	: D3D11App(hInstance)
 {
 	mClientWidth = nWidth;
 	mClientHeight = nHeight;
 
+	m_Curve = AnimationCurve::Cubic(-507 + 20, 280, 507 - 20, -280);
+
+	m_Curve->AddKey(-400, -100);
+	m_Curve->AddKey(100, 200);
 }
 
 BaseDemo::~BaseDemo()
@@ -33,6 +39,82 @@ void BaseDemo::DrawScene()
 {
 	SwapChainPtr->Begin();
 	ShowBlock(100, 100, 300, 200, { 0, 0, 1, 1 }, mTimer.TotalTime());
+
+
+
+# define NDIM 1
+# define NDATA 3
+	int left;
+	double xdata[NDATA];
+	double xval;
+	double ydata[NDIM*NDATA];
+	double yval[NDIM];
+	//y = -(x)**2
+	xdata[0] = -507;
+	ydata[0] = 300;
+	xdata[1] = 0;
+	ydata[1] = 0;
+	xdata[2] = 107;
+	ydata[2] = 300;
+	xval = -200.0;
+	DirectX::VertexPositionColor vertices[6000];
+
+	int i = 0;
+	for (double x = -mClientWidth / 2; (x < mClientHeight / 2) && (i < 1445); x++)
+	{
+		xval = x;
+		left = i;
+		if (NDATA - 2 < left)
+		{
+			left = NDATA - 2;
+		}
+		if (left < 1)
+		{
+			left = 1;
+		}
+		double yval1 = m_Curve->Evaluate(xval);
+		parabola_val2(NDIM, NDATA, xdata, ydata, left, (double)xval, yval);
+		double xtmp = xval;// xval / 28 * 10.f;
+		double ytmp = yval[0];// yval[0] / nSceneHeight / 28 * 10.0f;
+		ytmp = yval1;
+		xtmp = xval + mClientWidth / 2;
+		ytmp = mClientHeight / 2 - ytmp;
+
+		vertices[i] = DirectX::VertexPositionColor(XMFLOAT3((float)xtmp, ytmp, 0.0f), XMFLOAT4(0.5f, 1.0f, 0.5f, 1));
+		i++;
+	}
+	XMMATRIX toTexSpace(
+		0.5f * mClientWidth, 0.0f, 0.0f, 0.0f,
+		0.0f, -0.5f * mClientHeight, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.5f * mClientWidth, 0.5f * mClientHeight, 0.0f, 1.0f);
+
+	int tmpX = (float)mClientWidth*1.0f / 2.0f;
+	int tmpY = (float)mClientHeight*1.0f / 2.0f;
+	int x = vertices[0].position.x;
+	int y = vertices[0].position.y;
+	tmpX -= x;
+	tmpY -= y;
+
+	XMMATRIX m_OrthotMat = XMMatrixOrthographicLH((float)mClientWidth, (float)mClientHeight, 0.0f, 1.0f);
+	static int kkkkkk = 0;
+	if (kkkkkk > mClientWidth - 1)
+	{
+		kkkkkk = 0;
+	}
+	DrawPrimitiveUP(PRIMITIVE_LINESTRIP, mClientWidth, vertices,m_OrthotMat);
+	int nPosX = vertices[kkkkkk].position.x;// +nSceneWidth / 2;
+	int nPosY = vertices[kkkkkk].position.y;
+
+	ShowBlock(nPosX - 5, nPosY - 5, nPosX + 5, nPosY + 5, XMFLOAT4(DirectX::Colors::Red));
+	kkkkkk++;
+	for (int i = 0; i < m_Curve->Size(); ++i)
+	{
+		int xPos = m_Curve->Key(i) + mClientWidth / 2;
+		int yPos = mClientHeight / 2 - m_Curve->Value(i);
+		ShowBlock(xPos - 5, yPos - 5, xPos + 5, yPos + 5, XMFLOAT4(DirectX::Colors::Blue));
+	}
+
 	SwapChainPtr->Flip();
 }
 
@@ -79,27 +161,7 @@ void BaseDemo::ShowBlock(int x1, int y1, int x2, int y2, const XMFLOAT4& color, 
 	//rTmp = XMMatrixInverse(&pDeterminant, rTmp );
 	toTexSpace = XMMatrixInverse(&pDeterminant, toTexSpace);
 	toTexSpace = rTmp * toTexSpace;
-
-
-	D3D11_MAPPED_SUBRESOURCE mappedResource;
-	ZeroMemory(&mappedResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
-	m_deviceContext->Map(m_vertexBuffer,
-		0,
-		D3D11_MAP_WRITE_DISCARD,
-		0,
-		&mappedResource);
-	memcpy(mappedResource.pData, vertices, 6 * sizeof(VertexPositionColorTexture));
-	m_deviceContext->Unmap(m_vertexBuffer, 0);
-
-	unsigned int stride;
-	unsigned int offset;
-	stride = sizeof(VertexPositionColorTexture);
-	offset = 0;
-	g_objShaders.BaseShader()->SetShaderParameters(XMMatrixIdentity(), XMMatrixIdentity(), toTexSpace, srv);
-	g_objShaders.BaseShader()->RenderShader();
-	m_deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
-	m_deviceContext->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	m_deviceContext->Draw(6, 0);
+	DrawPrimitiveUP(PRIMITIVE_TRIANGLELIST, 6, vertices, toTexSpace);
 }
 
 void BaseDemo::OnResize()
@@ -119,7 +181,7 @@ void BaseDemo::InitResource()
 
 	D3D11_BUFFER_DESC vertexBufferDesc;
 	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC; //动态缓存
-	vertexBufferDesc.ByteWidth = sizeof(VertexPositionColorTexture) * 500;
+	vertexBufferDesc.ByteWidth = sizeof(VertexPositionColorTexture) * 10000;
 	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; //CPU 可写
 	vertexBufferDesc.MiscFlags = 0;
@@ -129,3 +191,53 @@ void BaseDemo::InitResource()
 	result = m_d3dDevice->CreateBuffer(&vertexBufferDesc, NULL, &m_vertexBuffer);
 
 }
+void BaseDemo::DrawPrimitiveUP(PrimitiveType PrimitiveType, unsigned int PrimitiveCount, VertexPositionColorTexture *pVertexs, const XMMATRIX &model /*= XMMatrixIdentity()*/, ID3D11ShaderResourceView*pTexture /*= NULL*/)
+{
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	ZeroMemory(&mappedResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+	m_deviceContext->Map(m_vertexBuffer,
+		0,
+		D3D11_MAP_WRITE_DISCARD,
+		0,
+		&mappedResource);
+	//	Update the vertex buffer here.
+	int nSize = PrimitiveCount;
+	if (nSize > 10000)
+	{
+		nSize = 10000;
+	}
+	memcpy(mappedResource.pData, pVertexs, nSize * sizeof(VertexPositionColorTexture));
+	//	Reenable GPU access to the vertex buffer data.
+	m_deviceContext->Unmap(m_vertexBuffer, 0);
+
+	unsigned int stride;
+	unsigned int offset;
+	stride = sizeof(VertexPositionColorTexture);
+	offset = 0;
+
+
+
+	//1输入布局阶段
+	g_objShaders.BaseShader()->SetShaderParameters(XMMatrixIdentity(), XMMatrixIdentity(), model, pTexture);
+	g_objShaders.BaseShader()->RenderShader();
+
+	m_deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+	m_deviceContext->IASetPrimitiveTopology((D3D_PRIMITIVE_TOPOLOGY)PrimitiveType);
+
+
+	m_deviceContext->Draw(PrimitiveCount, 0);
+}
+
+void BaseDemo::DrawPrimitiveUP(PrimitiveType PrimitiveType, unsigned int PrimitiveCount, VertexPositionColor *pVertexs, const XMMATRIX &model /*= XMMatrixIdentity()*/, ID3D11ShaderResourceView*pTexture /*= NULL*/)
+{
+	std::vector<VertexPositionColorTexture> vertex;
+	vertex.resize(PrimitiveCount);
+	for (int i = 0; i < PrimitiveCount; ++i)
+	{
+		VertexPositionColorTexture& pPoint = vertex[i];
+		pPoint.color = pVertexs[i].color;
+		pPoint.position = pVertexs[i].position;
+	}
+	DrawPrimitiveUP(PrimitiveType, PrimitiveCount, &vertex[0], model, pTexture);
+}
+
--- D3D11Demo/D3D11Demo/BaseDemo.h
+++ D3D11Demo/D3D11Demo/BaseDemo.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "D3D11App.h"
 #include "HpD3D9Type.h"
-
+#include "VertexTypes.h"
 enum PrimitiveType
 {
 	PRIMITIVE_UNDEFINED = 0,
@@ -22,11 +22,21 @@ public:
 	virtual void DrawScene();
 	virtual void OnResize();
 	void ShowBlock(int x1, int y1, int x2, int y2, const XMFLOAT4& color, float dt = 0);
+
+	void DrawPrimitiveUP(PrimitiveType PrimitiveType, unsigned int PrimitiveCount,
+		VertexPositionColor *pVertexs, const XMMATRIX &model = XMMatrixIdentity(), ID3D11ShaderResourceView*pTexture = NULL);
+
+	void DrawPrimitiveUP(PrimitiveType PrimitiveType, unsigned int PrimitiveCount,
+		VertexPositionColorTexture *pVertexs, const XMMATRIX &model = XMMatrixIdentity(), ID3D11ShaderResourceView*pTexture = NULL);
+
+
+
 protected:
 	void InitResource();
 private:
 	ID3D11Buffer	*m_vertexBuffer;
 	ID3D11Buffer	*m_indexBuffer;
 	ID3D11ShaderResourceView* srv;
+	std::shared_ptr<class AnimationCurve> m_Curve;
 };
 
